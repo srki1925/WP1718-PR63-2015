71c25d7be51243d81d41edd85c9486b43f32222c
Srdjan Zivkovic <srki1925@gmail.com>
2018-06-21 01:47:55 +0200

    registration username, password, email and phone validation

--- TaxiService/TaxiService/AngularApp/TaxiServiceSPA/src/app/components/users/registration/registration.component.ts
+++ TaxiService/TaxiService/AngularApp/TaxiServiceSPA/src/app/components/users/registration/registration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ICar } from '../../../services/interfaces';
+import { CarsDataService } from '../../../services/cars-data.service';
 
 @Component({
   selector: 'app-registration',
@@ -12,26 +14,30 @@ export class RegistrationComponent implements OnInit, OnDestroy {
 
   registrationForm: FormGroup;
   isDriver = false;
+  freeCars : ICar[];
   private typeSubscription:Subscription;
   constructor(private route:ActivatedRoute,
-              private router:Router) { }
+              private router:Router,
+              private carsService:CarsDataService) { }
 
   ngOnInit() {
     //subscribe to query params to protect initialized component tampering trough query
     this.typeSubscription = this.route.queryParams.subscribe((query) =>{
       const type = query['type'];
-      console.log(type);
       if(!type || (type !== 'customer' && type !== 'driver')){
         this.router.navigate([''], {relativeTo:this.route, queryParams:{type:'customer'}});
       }else{
         if(type === 'driver'){
           this.isDriver = true;
+          
         }else{
           this.isDriver = false;
         }
       }
     });
 
+    this.freeCars = this.carsService.getFreeCars();
+    console.log(this.freeCars);
 
     this.registrationForm = new FormGroup({
       username: new FormControl(null, Validators.required),
@@ -40,9 +46,8 @@ export class RegistrationComponent implements OnInit, OnDestroy {
       name: new FormControl(null),
       lastname: new FormControl(null),
       jmbg: new FormControl(null),
-      phone: new FormControl(null, Validators.required),
-      type: new FormControl(1, Validators.required),
-      car: new FormControl(null),
+      phone: new FormControl(null, [Validators.required,Validators.pattern('[0-9]*')]),
+      carNumber: new FormControl(null),
     });
   }
 
--- TaxiService/TaxiService/AngularApp/TaxiServiceSPA/src/app/services/cars-data.service.ts
+++ TaxiService/TaxiService/AngularApp/TaxiServiceSPA/src/app/services/cars-data.service.ts
@@ -6,17 +6,16 @@ import { ICar, CarType } from './interfaces';
 })
 export class CarsDataService implements OnInit {
 
-  private cars:ICar[];
+  private cars:ICar[] = [
+    {carNumber:10, carType:CarType.sedan, registration:'ns-111-aa',  driverId:null, year:2010},
+    {carNumber:11, carType:CarType.van, registration:'ns-112-aa',  driverId:1, year:2011},
+    {carNumber:12, carType:CarType.sedan, registration:'ns-113-aa',  driverId:null, year:2012},
+    {carNumber:13, carType:CarType.van, registration:'ns-114-aa',  driverId:5, year:2013},
+  ];
 
   constructor() { }
 
   ngOnInit(){
-    this.cars = [
-      {carNumber:10, carType:CarType.sedan, registration:'ns-111-aa',  driverId:-1, year:2010},
-      {carNumber:11, carType:CarType.van, registration:'ns-112-aa',  driverId:-1, year:2011},
-      {carNumber:12, carType:CarType.sedan, registration:'ns-113-aa',  driverId:-1, year:2012},
-      {carNumber:13, carType:CarType.van, registration:'ns-114-aa',  driverId:-1, year:2013},
-    ];
   }
 
   addNewCar(newCar:ICar){
@@ -41,5 +40,15 @@ export class CarsDataService implements OnInit {
 
   getAllCars(){
     return this.cars.slice();
+  }
+
+  getFreeCars(){
+    let retCars : ICar[] = [];
+    this.cars.forEach((car:ICar) =>{
+      if(!car.driverId){
+        retCars.push(car);
+      }
+    });
+    return retCars;
   }
 }
